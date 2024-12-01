2020cd3fa49071b0b9993ea4555c515637758243
Cian O'Gralaigh <ihatecian@gmail.com>
2024-12-01 18:53:06 +0000

    new: add context modes for products remote

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,25 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el = "#dev-products") => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation: We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
