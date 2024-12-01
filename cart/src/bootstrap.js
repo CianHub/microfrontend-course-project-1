1edcdae00771ed0bffd0d641e322ca13034500ee
Cian O'Gralaigh <ihatecian@gmail.com>
2024-12-01 19:03:00 +0000

    new: update exposes array in cart, refactor how host app calls cart

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,4 +1,20 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-document.querySelector("#dev-cart").innerHTML = cartText;
+const mount = (el = "#dev-cart") => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
+// Context/Situation: We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-cart'
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
